f0f88e75a51eafdb7e60fd65262b6a448b3e8b8e
Marcelo Azevedo <marcellorhcp@gmail.com>
2021-09-27 23:19:07 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,33 +1,34 @@
-const fetchPokemon = () => {
-  const getPokemonUrl = id => `https://pokeapi.co/api/v2/pokemon/${id}`
+const getPokemonUrl = id => `https://pokeapi.co/api/v2/pokemon/${id}`
 
-  const pokemonPromisses = [] 
+const generatePokemonPromises = () => Array(150).fill().map((_, index ) => 
+fetch(getPokemonUrl(index + 1)).then(response => response.json()))
 
-  for (let i = 1; i <= 150; i++){
-    pokemonPromisses.push(fetch(getPokemonUrl(i)).then(response => response.json()))
-  }
+const generateHTML = pokemons => pokemons.reduce((accumulator, { name, id, types }) => {
+    const elementTypes = types.map(typeInfo => typeInfo.type.name)
 
-  Promise.all(pokemonPromisses)
-  .then(pokemons => {
-    const lisPokemons = pokemons.reduce((accumulator, pokemon) => {
-      const types = pokemon.types.map(typeInfo => typeInfo.type.name)
+  accumulator += `
+      <li class="card ${elementTypes[0]}">
+      <img class="card-image" alt="${name}" src="https://cdn.traction.one/pokedex/pokemon/${id}.png"/>
+        <h2 class= "card-title">${id}. ${name}</h2>
+        <p class="card-subtitle">${elementTypes.join(' | ')}</p>
+      </li>
+    `
+  return accumulator;
+}, '')
 
-      accumulator += `
-        <li class="card ${types[0]}">
-        <img class="card-image" alt="${pokemon.name}" src="https://cdn.traction.one/pokedex/pokemon/${pokemon.id}.png"/>
-          <h2 class= "card-title">${pokemon.id}. ${pokemon.name}</h2>
-          <p class="card-subtitle">${types.join(' | ')}</p>
-        </li>
-      `
 
-      return accumulator;
-    }, '')
-    const ul = document.querySelector('[data-js="pokedex"]')
-
-    ul.innerHTML = lisPokemons
-  })
+const insertPokemonsIntoPage = pokemons => {
+  const ul = document.querySelector('[data-js="pokedex"]')
+  ul.innerHTML = pokemons
 }
 
-fetchPokemon() 
+
+const pokemonPromisses = generatePokemonPromises()
+
+Promise.all(pokemonPromisses)
+  .then(generateHTML)
+  .then(insertPokemonsIntoPage)
+
+
 
 // parei em 18 minutos
